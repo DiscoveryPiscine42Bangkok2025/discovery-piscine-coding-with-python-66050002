b6c385f3a9931f32985a85cfb9b60785c9fc078f
66050002 <66050002@kmitl.ac.th>
2025-09-16 01:00:18 +0700

    Add files via upload

---
+++ cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name = "Kotchaporn"     
+last_name = "Poemcharoen"
+print(first_name, last_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Kotchaporn"  
+last_name = "Poemcharoen" 
+whole_name = first_name + " " + last_name
+print(whole_name)
